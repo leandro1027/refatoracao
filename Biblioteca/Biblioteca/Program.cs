e6c1004cd1efd757281ddc27337e5e7a09bcddce
leandro1027 <126489808+leandro1027@users.noreply.github.com>
2024-08-02 18:06:07 -0300

    .

--- Biblioteca/Biblioteca/Program.cs
+++ Biblioteca/Biblioteca/Program.cs
@@ -1,27 +1,43 @@
-﻿namespace Biblioteca;
-
-class Program
+﻿namespace Biblioteca
 {
-	static void Main(string[] args)
-	{
-		Biblioteca biblioteca= new Biblioteca();
-		
-		// adicionar cliente
-		biblioteca.clientes.Add(new Cliente{Id = 1, Nome = "Jorge e Mateus", 
-		DataNascimento = new DateTime(1980, 01, 01), Telefone = "62999999"});
-		
-		// adicionar livro
-		biblioteca.livros.Add(new Livro{Id = 1, Titulo ="Dom Casmurro", 
-		Autor = "Machado de Assis", Disponivel = true});
-		
-		// Emprestar livro
-		biblioteca.EmprestarLivro(1, 1);
-		biblioteca.DevolverLivro(1, 1);
-		biblioteca.EmprestarLivro(1, 2);
-		
-		/*static void IniciarBiblioteca(Biblioteca biblioteca)
-		{
-			biblioteca.EmprestarLivro(2,2);
-		}*/
-	}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var biblioteca = new Biblioteca();
+
+            AdicionarClientes(biblioteca);
+            AdicionarLivros(biblioteca);
+            EmprestarEDevolverLivros(biblioteca);
+        }
+
+        static void AdicionarClientes(Biblioteca biblioteca)
+        {
+            biblioteca.clientes.Add(new Cliente
+            {
+                Id = 1,
+                Nome = "Jorge e Mateus",
+                DataNascimento = new DateTime(1980, 01, 01),
+                Telefone = "62999999"
+            });
+        }
+
+        static void AdicionarLivros(Biblioteca biblioteca)
+        {
+            biblioteca.livros.Add(new Livro
+            {
+                Id = 1,
+                Titulo = "Dom Casmurro",
+                Autor = "Machado de Assis",
+                Disponivel = true
+            });
+        }
+
+        static void EmprestarEDevolverLivros(Biblioteca biblioteca)
+        {
+            biblioteca.EmprestarLivro(1, 1);
+            biblioteca.DevolverLivro(1, 1);
+            biblioteca.EmprestarLivro(1, 2);
+        }
+    }
 }
